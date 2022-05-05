42edcf18e7358cecc151a0c24d07f251839521ff
Beto <aaayza@gmail.com>
2022-05-05 19:07:22 -0300

    First commit

---
+++ jquery_logic.js
@@ -0,0 +1,9 @@
+import quotes from './logic.js';
+
+const randomElement=(array)=>{
+    return array.[Math.floor(Math.random()*array.length)];
+};
+
+$(document).ready(function(){
+    $("#text").append("<p>"+randomElement(quotes)+"</p>");
+});
