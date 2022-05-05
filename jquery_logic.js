5bc0d5f6b74200e30d6de2f5b90bc43be9d894e0
Alberto <aaayza@gmail.com>
2022-05-05 20:22:12 -0300

    Commit test2

--- jquery_logic.js
+++ jquery_logic.js
@@ -4,7 +4,9 @@ const randomElement=(array)=>{
     return array.[Math.floor(Math.random()*array.length)];
 };
 
-const asds=()=>{}
+const asds=()=>{
+    const vasdad= 123123;
+}
 
 $(document).ready(function(){
     $("#text").append("<p>"+randomElement(quotes)+"</p>");
